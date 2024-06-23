e6fb00718de558907b992f7a711980637fe36f88
chaichana <chaichana@theiconweb.com>
2024-06-23 18:22:49 +0700

    generate kanban  board and card

---
+++ src/app/modules/kanban-card/kanban-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-kanban-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './kanban-card.component.html',
+  styleUrl: './kanban-card.component.scss'
+})
+export class KanbanCardComponent {
+
+}
